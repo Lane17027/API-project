b7714281aa8177c55ce5a1f05a2f802500f7d53d
Lane17027 <lanenichols17027@gmail.com>
2023-10-30 22:37:55 -0600

    more user associations

--- backend/db/models/spot.js
+++ backend/db/models/spot.js
@@ -11,9 +11,7 @@ module.exports = (sequelize, DataTypes) => {
      * The `models/index` file will call this method automatically.
      */
     static associate(models) {
-      Spot.belongsTo(models.User, {foreignKey: 'id'}),
-
-      Spot.hasMany
+      Spot.belongsTo(models.User, {foreignKey: 'id'})
     }
   }
   Spot.init({
